acae1bed65fc87f1f5c4b4bf7f1321a4fd01797a
Yoan Mollard <ymollard@users.noreply.github.com>
2021-01-03 20:19:04 +0100

    CI script name

--- ci/deploy-to-netlify.sh
+++ ci/deploy-to-netlify.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 die(){ echo "Coudn't $*" >&2; exit 1; }
 
-echo -e "\e[36mStarting update_documentation.sh\e[39m: install deps"
+echo -e "\e[36mStarting $0\e[39m: install deps"
 
 npm install -g gitbook-cli || die "install gitbook-cli"
 gitbook install || die "run gitbook install"
 
-echo -e "\e[36mupdate_documentation.sh\e[39m: build"
+echo -e "\e[36m$0\e[39m: build"
 
 gitbook build -d || die "Build the documentation in HTML"
 
-echo -e "\e[36mupdate_documentation.sh\e[32m: BUILD SUCCEEDED!\e[39m"
+echo -e "\e[36m$0\e[32m: BUILD SUCCEEDED!\e[39m"
 
